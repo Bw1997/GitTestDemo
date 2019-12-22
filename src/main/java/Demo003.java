cd4ca4f99acc7e858de70384a4c20bc2db57f4e2
java0722 <test@qq.com>
2019-12-22 19:47:50 +0800

    小白的操作

--- src/main/java/Demo003.java
+++ src/main/java/Demo003.java
@@ -3,4 +3,17 @@ public class Demo003 {
     public static void main(String[] args) {
         System.out.println("开始发送短信");
     }
+
+    //小白新增方法1
+    public void test1(){
+        for(int i=1;i<=100;i++){
+            System.out.println("第"+i+"个数"+i);
+        }
+    }
+
+    //小白新增方法2
+    public void test2(){
+        System.out.println("甚是聒噪");
+    }
+
 }
