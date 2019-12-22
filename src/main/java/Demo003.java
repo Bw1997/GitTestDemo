aff0efe7df797af08968051dbc694f5963981151
java0722 <test@qq.com>
2019-12-22 19:52:25 +0800

    老鸟新增了方法test1/test2

--- src/main/java/Demo003.java
+++ src/main/java/Demo003.java
@@ -3,4 +3,14 @@ public class Demo003 {
     public static void main(String[] args) {
         System.out.println("开始发送短信");
     }
+
+    //老鸟新增的方法1
+    public void test1(){
+        System.out.println("老鸟的操作1");
+    }
+
+    //老鸟新增的方法2
+    public void test2(){
+        System.out.println("老鸟的操作2");
+    }
 }
